332e597665152ebc814a642fa858dd2ce604c732
Nobel <nabeelandnizam@gmail.com>
2024-04-09 10:27:38 +0300

    initial commit 🌱

--- README.md
+++ README.md
@@ -1,2 +1,137 @@
 # Badar
-Declarative, flexbox inspired GUI library for LÖVE 2D
+
+Declarative, flexbox inspired GUI library for LÖVE 2D.<br>
+Badar focuses on composition, auto-layout and reactivity; width, height, position auto-calculated (if not provided) based on parent properties.
+
+## Installation
+
+The `badar.lua` file should be dropped into an existing project and required by it.<br>
+
+Dependencies you should also add:
+
+- [classic](https://github.com/rxi/classic) to make it easier to build your own UI components.
+- [Hump:Signal](https://github.com/vrld/hump/blob/master/signal.lua) Pass mouse events for all elements.
+
+### Example
+
+```lua
+function love.load()
+    local container = require 'libs.badar'
+
+    local button = container({ width = 25, height = 25 }):color({ 1, 0, 0 })
+    local square = container({ width = 10, height = 10 }):color({ 0, 0, 1 }, true)
+
+    main = container({ width = screenWidth, height = screenHeight })
+        :content({
+            square,
+            button:onClick(function()
+                square:update(function(sq)
+                    sq.width = sq.width + 10;
+                    return sq
+                end)
+            end),
+        })
+        :column()
+        :padding({ 16, 16, 16, 16 })
+end
+
+function love.draw()
+    main:render()
+end
+```
+
+## Reactivity
+
+You can define your UI tree of nodes within `love.load` or `love.draw`.<br>
+
+- Use `load` function for animations purposes (e.g `flux`) or for readability.
+- Use `draw` for more 'reactivity' as you don't need to use `:update()` function to update values, but you need to define your component within `draw`
+
+## Functions
+
+### Creating new "container"
+
+```lua
+local container = require 'path.to.bardar.lua'
+local c = container()
+```
+
+Creates a new "container" to mange its "children". You can pass an optional table to define `x`, `y`, `width` and `height`. <br>
+Container are based on LÖve `rectangle`. <br>
+Container calculates its children width and height if width and height are not specified, using `autoLayout` table. You can set them to false, if you want to calculate them manually (e.g text width). <br>
+Using auto layout, the available space is distributed equally between children.
+
+```lua
+    autoLayout={x = self.width == 0, y = self.height == 0 }
+```
+
+You can define container position using `x` and `y` otherwise the are defined automatically.
+
+### :content({})
+
+Adds passed table as children to the container.
+
+### :center()
+
+Centers container's child.
+
+### :row(gap)
+
+Aligns children on the x-axis, with defined `gap` space between them. Default gap is 0.
+
+### :column(gap)
+
+Aligns children on the y-axis, with defined `gap` space between them. Default gap is 0.
+
+### :color(color, backgroundFilled)
+
+Sets color and draw mode of container. Second argument (boolean) is optional, default is `false`. <br>
+You can pass `canHover` to your container constructor to respond to `hover` without logic attached.
+
+### :padding({0,0,0,0})
+
+Adds padding. The order: Top → right → bottom → left
+
+### :onClick(fn)
+
+Sets the function fn to be called when mouse is clicked within the container.
+
+### update(function(o) return o end)
+
+Provides a way to change container properties. Can be used for animation and reactivity purposes.<br>
+You must return the argument.
+
+```lua
+container():update(function(o)
+    o.canHover= true
+    return o
+end)
+```
+
+### :render()
+
+Calls container and its children `draw` function
+
+### :draw()
+
+Used to override `draw` function when creating your own component. <br>
+This function return another function to make sure the drawing order of children; use `:render()` as it is syntax sugar for `:draw()()`.
+
+```lua
+text = badar:extend()
+function text:new(text)
+    badar.new(self)
+    self.text = text
+end
+function text:draw()
+    love.graphics.print(self.text, self.x, self.y)
+    return function()
+        return self
+    end
+end
+```
+
+## License
+
+This library is free software; you can redistribute it and/or modify it under
+the terms of the MIT license. See [LICENSE](LICENSE) for details.
---
+++ badar.lua
@@ -0,0 +1,209 @@
+--
+-- badar
+--
+-- Copyright (c) 2024 Nabeel
+--
+-- This library is free software; you can redistribute it and/or modify it
+-- under the terms of the MIT license. See LICENSE for details.
+--
+
+
+badar = Object:extend()
+
+local function isPointInside(px, py, rect)
+    px, py = px or 0, py or 0
+    rect = rect or { 0, 0, 0, 0 }
+
+    local rx1, ry1, rx2, ry2 = rect[1], rect[2], rect[3], rect[4]
+    return px >= rx1 and px <= rx2 and py >= ry1 and py <= ry2
+end
+
+local function handleLayout(self)
+    local results = 0;
+    local total_width = self.width - self._padding[4] - self._padding[2] - (self.gap * (#self.children - 1))
+    local total_height = self.height - self._padding[1] - self._padding[3] - (self.gap * (#self.children - 1))
+    for _, child in ipairs(self.children) do
+        if child.autoLayout.x or child.autoLayout.y then
+            results = results + 1
+        else
+            total_width = total_width - child.width
+            total_height = total_height - child.height
+        end
+    end
+
+    return results, { width = total_width, height = total_height }
+end
+
+
+function badar:new(obj)
+    obj = obj or {}
+
+    self.x = obj.x or 0
+    self.y = obj.y or 0
+    self.width = obj.width or 0
+    self.height = obj.height or 0
+    self.autoLayout = { x = self.width == 0, y = self.height == 0 } -- to calculate dimensions automatically
+
+    self._padding = obj.padding or { 0, 0, 0, 0 }                   -- top, right. bottom, left
+    self._center = false
+    self._row = false;
+    self._column = false;
+    self.gap = 0;
+
+    self.hovered = false
+    self.canHover = obj.canHover or false
+    self.globalPosition = { x = 0, y = 0 }
+    self.clickLogic = obj.onClick or function() end;
+
+    self.children = obj.children or {}
+    self._color = obj.color or { 1, 1, 1, 0 }
+    self.background = obj.background or false;
+
+    Signal.register('mouseEvent', function(mx, my)
+        self:onHover(mx, my)
+    end)
+    Signal.register('mousePressed', function(mx, my)
+        self:handleClick(mx, my)
+    end)
+    return self
+end
+
+function badar:draw()
+    love.graphics.setColor(self._color)
+    love.graphics.rectangle(
+        self:isHover(),
+        self.x,
+        self.y,
+        self.width,
+        self.height)
+
+    return function()
+        love.graphics.push()
+        love.graphics.translate(math.floor(self.x + self._padding[4]), math.floor(self.y + self._padding[1]))
+        self.globalPosition.x, self.globalPosition.y = love.graphics.inverseTransformPoint(screenWidth, screenHeight)
+        self.globalPosition.x = screenWidth - self.globalPosition.x
+        self.globalPosition.y = screenHeight - self.globalPosition.y
+
+        local childrenNumber, available_space = handleLayout(self)
+        local offset = 0;
+
+        for _, child in ipairs(self.children) do
+            if self._center then
+                child.x = (self.width - child.width) / 2 - self._padding[4] - self._padding[2]
+                child.y = (self.height - child.height) / 2 - self._padding[1] - self._padding[3]
+            end
+
+            if self._row then
+                child.x = math.floor(offset);
+                offset = offset + child.width + self.gap
+
+                if child.autoLayout.x then
+                    child.width = math.floor((1 / childrenNumber) * available_space.width)
+                end
+                if child.autoLayout.y then
+                    child.height = self.height - self._padding[1] - self._padding[3]
+                end
+            end
+
+            if self._column then
+                child.y = math.floor(offset);
+                offset = offset + child.height + self.gap
+
+                if child.autoLayout.x then
+                    child.width = self.width - self._padding[4] - self._padding[2]
+                end
+                if child.autoLayout.y then
+                    child.height = (1 / childrenNumber) * available_space.height
+                end
+            end
+            child:draw()()
+        end
+        love.graphics.pop()
+        return self
+    end
+end
+
+function badar:center()
+    self._center = true
+    return self;
+end
+
+function badar:row(gap)
+    gap = gap or 0
+    self.gap = gap
+    self._row = true
+    return self;
+end
+
+function badar:column(gap)
+    gap = gap or 0;
+    self.gap = gap
+    self._column = true
+    return self;
+end
+
+function badar:content(content)
+    self.children = content;
+    return self;
+end
+
+function badar:onHover(mx, my)
+    if isPointInside(mx, my, self:getRect()) then
+        self.hovered = true
+    else
+        self.hovered = false
+    end
+end
+
+function badar:isHover()
+    if self.hovered and self.canHover then
+        return 'fill'
+    end
+    if self.background then
+        return 'fill'
+    end
+    return 'line'
+end
+
+function badar:handleClick(mx, my)
+    if isPointInside(mx, my, self:getRect()) then
+        self.clickLogic()
+        self.hovered = true
+    else
+        self.hovered = false
+    end
+end
+
+function badar:onClick(command)
+    self.clickLogic = command
+    self.canHover = true
+    return self
+end
+
+function badar:getRect()
+    return { self.globalPosition.x, self.globalPosition.y, self.globalPosition.x + self.width, self.globalPosition.y +
+    self.height }
+end
+
+function badar:render()
+    return self:draw()()
+end
+
+function badar:color(color, isFilled)
+    isFilled = isFilled or false
+    self._color = color
+    self.background = isFilled
+    return self
+end
+
+function badar:padding(padding)
+    self._padding = padding
+    return self
+end
+
+function badar:update(func)
+    self = func(self)
+    return self
+end
+
+return badar
