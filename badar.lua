9680f52d0518116330c3e4c43ed9f55669dec86c
Nobel <nabeelandnizam@gmail.com>
2024-04-22 15:31:37 +0300

    add scale to styles

--- badar.lua
+++ badar.lua
@@ -26,6 +26,7 @@ function badar:new(obj)
         corner = 0,
         opacity = 1, -- set to zero to hide border
         filled = false,
+        scale = 1
     }
     self.gap = 0;
     self.hideBorder = obj.hideBorder or false;
@@ -55,6 +56,7 @@ function badar:new(obj)
 end
 
 function badar:draw()
+    love.graphics.scale(self._style.scale)
     love.graphics.setColor({ self._style.color[1], self._style.color[2], self._style.color[3], self._style.opacity })
     self.drawFunc()
 
