91163a0ba6f93acd06762dc5c5f2ce9728a3078b
Nobel <nabeelandnizam@gmail.com>
2024-04-24 14:51:39 +0300

    Replace string file path with image resource

--- components/icon.lua
+++ components/icon.lua
@@ -1,56 +1,40 @@
 local badar = require 'badar'
 icon = badar:extend()
 
-function icon:new(fileName, obj)
+function icon:new(image, obj)
     obj = obj or {}
-    fileName = fileName or ''
     icon.super.new(self, obj)
-    local iconStyle = {
-        tint = false,
-        scale = 1,
-    }
-    extend(self._style, iconStyle)
+    if type(image) ~= "userdata" then
+        print('ERROR. Badar. Missing argument for icon component, no image resource.')
+        return self
+    end
+
     local horizontalPadding = self._style.padding[4] + self._style.padding[2]
     local verticalPadding = self._style.padding[1] + self._style.padding[3]
-    if fileName == '' then return self end
-    self.image = love.graphics.newImage(fileName)
-    self.width = (self.image:getWidth() * self._style.scale) + horizontalPadding
-    self.height = (self.image:getHeight() * self._style.scale) + verticalPadding
+    self.image = image
 
-    self.drawFunc = function()
-        if self._style.tint then
-            love.graphics.setColor({
-                self._style.color[1],
-                self._style.color[2],
-                self._style.color[3],
-                self._style.opacity
-            })
+    if type(image) == 'userdata' and image:typeOf('Image') then
+        self.image = image
+        self.width = (self.image:getWidth() * self._style.scale) + horizontalPadding
+        self.height = (self.image:getHeight() * self._style.scale) + verticalPadding
+
+        self.drawFunc = function()
+            love.graphics.draw(
+                self.image,
+                self.x + self._style.padding[4],
+                self.y + self._style.padding[1],
+                0,
+                self._style.scale,
+                self._style.scale
+            )
         end
-
-        love.graphics.draw(
-            self.image,
-            self.x + self._style.padding[4],
-            self.y + self._style.padding[1],
-            0,
-            self._style.scale,
-            self._style.scale
-        )
+    else
+        self.drawFunc = function()
+        end
     end
+
 
     return self
-end
-
--- Copyright (c) 2020 rxi
-function extend(t, ...)
-    for i = 1, select("#", ...) do
-        local x = select(i, ...)
-        if x then
-            for k, v in pairs(x) do
-                t[k] = v
-            end
-        end
-    end
-    return t
 end
 
 return icon
