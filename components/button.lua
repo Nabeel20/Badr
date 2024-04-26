82a72756e7fa05f48012c3ad3d251faf2640f2d7
Nobel <nabeelandnizam@gmail.com>
2024-04-26 07:37:44 +0300

    Create button.lua

---
+++ components/button.lua
@@ -0,0 +1,104 @@
+local container = require 'badar'
+
+local button = function(txt, options)
+    options = options or {}
+
+    local _style = {
+        padding = { 8, 12, 8, 12 },
+        hoverEnabled = true,
+        color = Hex('#00000'),
+        corner = 4,
+        textColor = Hex("#ffffff"),
+        iconColor = Hex("#ffffff"),
+        filled = true,
+    }
+    local _layout = {
+        direction = 'row',
+        alignment = 'center',
+        gap = 4,
+    }
+
+    if options.variant == 'secondary' then
+        _style.color = Hex('#f4f4f5')
+        _style.textColor = Hex("#000000")
+        _style.iconColor = Hex("#000000")
+    end
+    if options.variant == 'destructive' then
+        _style.color = Hex('#dc2626')
+        _style.textColor = Hex("#ffffff")
+        _style.iconColor = Hex("#ffffff")
+    end
+    if options.variant == 'outline' or options.variant == 'icon' then
+        _style.filled = false
+        _style.color = Hex('#e5e5e5')
+        _style.textColor = Hex('#000000')
+        _style.iconColor = Hex('#000000')
+    end
+    if options.variant == 'ghost' then
+        _style.opacity = 0
+        _style.filled = false
+        _style.color = Hex('#e5e5e5')
+        _style.textColor = Hex("#000000")
+        _style.iconColor = Hex("#000000")
+    end
+    if options.disabled then
+        _style.color = Hex('#e4e4e7')
+        _style.textColor = Hex('#ffffff')
+        _style.iconColor = Hex('#ffffff')
+    end
+    local _content = {
+        text(txt):style({
+            color = _style.textColor,
+        })
+    }
+
+    if options.icon then
+        table.insert(_content, icon(options.icon):style({
+            color = _style.iconColor
+        }))
+    end
+    if txt == '' then
+        _content = {
+            icon(options.icon):style({
+                color = _style.iconColor
+            })
+        }
+        _layout = { centered = true }
+    end
+    return container({
+            data = {
+                disabled = options.disabled
+            }
+        })
+        :content(_content)
+        :style(_style)
+        :layout(_layout)
+        :onHover({
+            onEnter = function(i)
+                if not options.disabled then
+                    love.mouse.setCursor(love.mouse.getSystemCursor('hand'))
+                    i.mouseCaptured = true;
+                end
+                if options.variant == 'ghost' then
+                    i._style.opacity = 1
+                end
+            end,
+            onExit = function(i)
+                if i.mouseCaptured then
+                    love.mouse.setCursor()
+                    i.mouseCaptured = false
+                end
+                if options.variant == 'ghost' then
+                    i._style.opacity = 0
+                end
+            end
+        })
+end
+
+-- https://github.com/s-walrus/hex2color/blob/master/hex2color.lua
+function Hex(hex, value)
+    return { tonumber(string.sub(hex, 2, 3), 16) / 256, tonumber(string.sub(hex, 4, 5), 16) / 256, tonumber(
+        string.sub(hex, 6, 7), 16) / 256, value or 1 }
+end
+
+return button
