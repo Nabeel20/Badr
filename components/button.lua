72c6dbe1b1d8cdf77487595067166c2db27b56f3
Nobel <nabeelandnizam@gmail.com>
2024-04-29 10:56:40 +0300

    refactor button props

--- components/button.lua
+++ components/button.lua
@@ -5,12 +5,10 @@ local button = function(txt, options)
 
     local _style = {
         padding = { 8, 12, 8, 12 },
-        hoverEnabled = true,
         color = Hex('#00000'),
         corner = 4,
         textColor = Hex("#ffffff"),
         iconColor = Hex("#ffffff"),
-        filled = true,
     }
     local _layout = {
         direction = 'row',
@@ -25,19 +23,21 @@ local button = function(txt, options)
     end
     if options.variant == 'destructive' then
         _style.color = Hex('#dc2626')
+        _style.hoverColor = Hex('#df3b3b')
         _style.textColor = Hex("#ffffff")
         _style.iconColor = Hex("#ffffff")
     end
     if options.variant == 'outline' or options.variant == 'icon' then
-        _style.filled = false
-        _style.color = Hex('#e5e5e5')
+        _style.color = Hex('#ffffff')
+        _style.hoverColor = Hex('#f5f5f5')
+        _style.borderColor = Hex('#e5e5e5')
+        _style.borderWidth = 2
         _style.textColor = Hex('#000000')
         _style.iconColor = Hex('#000000')
     end
     if options.variant == 'ghost' then
         _style.opacity = 0
-        _style.filled = false
-        _style.color = Hex('#e5e5e5')
+        _style.hoverColor = Hex('#e5e5e5')
         _style.textColor = Hex("#000000")
         _style.iconColor = Hex("#000000")
     end
@@ -97,8 +97,11 @@ end
 
 -- https://github.com/s-walrus/hex2color/blob/master/hex2color.lua
 function Hex(hex, value)
-    return { tonumber(string.sub(hex, 2, 3), 16) / 256, tonumber(string.sub(hex, 4, 5), 16) / 256, tonumber(
-        string.sub(hex, 6, 7), 16) / 256, value or 1 }
+    return {
+        tonumber(string.sub(hex, 2, 3), 16) / 256,
+        tonumber(string.sub(hex, 4, 5), 16) / 256,
+        tonumber(string.sub(hex, 6, 7), 16) / 256,
+        value or 1 }
 end
 
 return button
