f918e037321986ee13342829d8f79d0be9abe1fb
Nobel <nabeelandnizam@gmail.com>
2024-05-07 18:47:57 +0300

    Refactor text component

--- components/text.lua
+++ components/text.lua
@@ -1,88 +1,71 @@
-local badar = require 'badar'
-text = badar:extend()
+local container = require 'badar'
 
-function text:new(txt, obj)
-    obj = obj or {}
-    text.super.new(self, obj)
-    local textStyle = {
+local text = function(txt, options)
+    local self = container(table.spread({})(options or {}));
+
+    -- override draw function
+    ---@diagnostic disable-next-line: duplicate-set-field
+    self.drawSelf = function()
+        self._style.limit = self.font:getWidth(self.text)
+        love.graphics.setFont(self.font)
+        self.font:setFilter("nearest", "nearest")
+        self.font:setLineHeight(self._style.lineHeight)
+        --text
+        love.graphics.setColor({
+            self._style.color[1],
+            self._style.color[2],
+            self._style.color[3],
+            self._style.opacity
+        })
+        love.graphics.printf(
+            self.text,
+            self.x + self._style.padding[4],
+            self.y + self._style.padding[1],
+            self._style.limit,
+            self._style.alignment
+        )
+    end
+
+    -- expand styles
+    self._style = table.spread(self._style)({
         color = { 0, 0, 0 }, -- default is black
         size = 14,
-        fontFamily = 'assets/Poppins-Regular.ttf',
         lineHeight = 1,
         alignment = 'left',
+        fontFamily = 'assets/Poppins-Regular.ttf',
         limit = 1000,
-    }
-    extend(self._style, textStyle)
-    if self._style.fontFamily ~= '' then
+    })
+    if self._style.fontFamily then
         self.font = love.graphics.newFont(self._style.fontFamily, self._style.size)
     else
         self.font = love.graphics.newFont(self._style.size)
     end
-    self._text = txt;
-    self.glyphWidth = 0
 
-    self.pressed = false;
-    self.selection = {
-        start  = 0,
-        finish = 0,
-        width  = 0,
-        height = 0,
+    self.text = txt;
+    local padding = {
+        horizontal = self._style.padding[2] + self._style.padding[4],
+        vertical = self._style.padding[1] + self._style.padding[3],
     }
-    if type(txt) == "string" then
-        local hPadding = self._style.padding[2] + self._style.padding[4]
-        local vPadding = self._style.padding[1] + self._style.padding[3]
-        self.width = self.font:getWidth(self._text) + hPadding
-        self.height = self.font:getHeight(self._text) + vPadding
 
-        self.drawSelf = function()
-            self._style.limit = self.font:getWidth(self._text)
-            love.graphics.setFont(self.font)
-            self.font:setFilter("nearest", "nearest")
-            self.font:setLineHeight(self._style.lineHeight)
-            --text
-            love.graphics.setColor({
-                self._style.color[1],
-                self._style.color[2],
-                self._style.color[3],
-                self._style.opacity
-            })
-            love.graphics.printf(
-                self._text,
-                self.x + self._style.padding[4],
-                self.y + self._style.padding[1],
-                self._style.limit,
-                self._style.alignment
-            )
+    self.width = self.font:getWidth(self.text) + padding.horizontal
+    self.height = self.font:getHeight(self.text) + padding.vertical
+
+    -- override style function
+    ---@diagnostic disable-next-line: duplicate-set-field
+    self.style = function(style)
+        for key, value in pairs(style) do
+            self._style[key] = value
         end
-    end
-
-
-    return self
-end
-
-function text:style(style)
-    text.super.style(self, style)
-    if self._style.fontFamily ~= '' then
-        self.font = love.graphics.newFont(self._style.fontFamily, self._style.size)
-    else
-        self.font = love.graphics.newFont(self._style.size)
-    end
-    self.width = self.font:getWidth(self._text) + self._style.padding[2] + self._style.padding[4]
-    self.height = self.font:getHeight(self._text) + self._style.padding[1] + self._style.padding[3]
-    return self
-end
-
--- https://github.com/rxi/lume/blob/master/lume.lua
-function extend(t, ...)
-    for i = 1, select("#", ...) do
-        local x = select(i, ...)
-        if x then
-            for k, v in pairs(x) do
-                t[k] = v
-            end
+        if self._style.fontFamily then
+            self.font = love.graphics.newFont(self._style.fontFamily, self._style.size)
+        else
+            self.font = love.graphics.newFont(self._style.size)
         end
+        self.width = self.font:getWidth(self.text) + padding.horizontal
+        self.height = self.font:getHeight(self.text) + padding.vertical
+        return self
     end
-    return t
+    return self
 end
 
 return text
