8714ac30c20577250f940e6def07de8640c037f1
Nobel <nabeelandnizam@gmail.com>
2024-05-08 13:15:54 +0300

    rewrite progress

--- components/progress.lua
+++ components/progress.lua
@@ -8,33 +8,28 @@ local progress = function(options)
             height = 8,
             id = 'track'
         })
-        :style({
+        .style({
             color = options.trackColor or { 0, 0, 0 },
             corner = 4,
             visible = false
         })
 
-    if options.value then track:style({ visible = true }) end
+    if options.value then track.style({ visible = true }) end
 
-    local props = {
-        width = options.width or 100,
-        height = 8,
-        value = options.value or 0
-    }
-    for key, value in pairs(options) do
-        props[key] = value
-    end
-
-    local output = container(props)
-        :content({ track })
-        :style({
+    local output = container(table.spread({
+            width = options.width or 100,
+            value = options.value or 0,
+            height = 8,
+        }, options or {}))
+        .content({ track })
+        .style({
             color = options.backgroundColor or { 0.89453125, 0.89453125, 0.89453125, 1 },
             filled = true,
             corner = 4
         })
 
-    output.setValue = function(_s, value)
-        local t = output:find('track'):style({ visible = true })
+    output.setValue = function(value)
+        local t = output.find('track').style({ visible = true })
         local nextValue = math.min(value, output.width)
         flux.to(t, 0.8, { width = nextValue }):ease('quartout')
         output.value = nextValue
