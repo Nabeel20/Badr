128a98b44f7b6a5159ea9388912c2206f947f584
Nobel <nabeelandnizam@gmail.com>
2024-05-08 13:12:13 +0300

    refactor checkbox

--- components/checkbox.lua
+++ components/checkbox.lua
@@ -1,45 +1,47 @@
 local container = require 'badar'
+local icon = require 'components.icon'
+local text = require 'components.text'
 local checkIcon = love.graphics.newImage('assets/check-line.png') -- https://remixicon.com/icon/check-line
-
-local box = container({
-    width = 16,
-    height = 16,
-    id = 'box'
-}):style({
-    borderWidth = 1,
-    corner = 4,
-    opacity = 0
-}):content({
-    icon(checkIcon, { id = 'icon' }):style({ opacity = 0 })
-})
-
 
 local checkbox = function(string, options)
     options = options or {}
-    options.disabled = options.disabled or false
     if options.disabled then options.color = Hex('#525252') end
-    box:style({
-        color = options.color or { 0, 0, 0 },
-        borderColor = options.color or { 0, 0, 0 },
-        opacity = 0
-    })
+    local self = container(options or {})
+
+    local box = container({
+            width = 16,
+            height = 16,
+            id = 'box'
+        })
+        .style({
+            borderWidth = 1,
+            corner = 4,
+            opacity = 0,
+            color = options.color or { 0, 0, 0 },
+            borderColor = options.color or { 0, 0, 0 },
+        })
+        .content({ icon(checkIcon, { id = 'icon' }).style({ opacity = 0 }) })
 
     if options.value then
-        box:style({ opacity = 1 }):find('icon'):style({ opacity = 1 })
+        box.style({ opacity = 1 }).find('icon').style({ opacity = 1 })
     end
-    local output = container({ value = options.value or false })
-        :content({
-            box,
-            text(string):style({ color = options.color })
-        })
-        :style({ opacity = 0 })
-        :layout({
+
+    self.onValueChange = function(func)
+        self._onValueChange = func
+        return self
+    end
+    local checkboxText = text(string).style({ color = options.color, size = options.size or 16 })
+    self.width         = box.width + checkboxText.width;
+    self.height        = box.height + checkboxText.height
+
+    self.content({ box, checkboxText },
+        {
             direction = 'row',
             alignment = 'center',
             gap = 8
         })
-        :onClick(function(i)
-            print('checkBox clicked', i.value)
+        .style({ opacity = 0 })
+        .onClick(function(i)
             if not options.disabled then
                 local opacity = 1
                 i.value = not i.value
@@ -49,14 +51,14 @@ local checkbox = function(string, options)
                 else
                     opacity = 0
                 end
-                box:style({ opacity = opacity }):find('icon'):style({ opacity = opacity })
+                box.style({ opacity = opacity }).find('icon').style({ opacity = opacity })
 
                 if i._onValueChange then
                     i._onValueChange(i.value, string)
                 end
             end
         end)
-        :onHover({
+        .onHover({
             onEnter = function()
                 if options.disabled then
                     love.mouse.setCursor(love.mouse.getSystemCursor('no'))
@@ -68,12 +70,8 @@ local checkbox = function(string, options)
                 love.mouse.setCursor()
             end
         })
-    output.onValueChange = function(_s, func)
-        output._onValueChange = func
-        return output
-    end
 
-    return output
+    return self
 end
 
 return checkbox
