c5de442b37d9baa7542c74aa2adf40f13c0291bb
Nobel <nabeelandnizam@gmail.com>
2024-05-09 11:56:48 +0300

    Refactor icon

--- components/icon.lua
+++ components/icon.lua
@@ -1,28 +1,26 @@
 local container = require 'badar'
 
 local icon = function(image, options)
-    local self = container(options)
+    local self = container(options or {})
 
     local padding = {
         horizontal = self._style.padding[4] + self._style.padding[2],
         vertical = self._style.padding[1] + self._style.padding[3]
     }
+    self.image = image
+    self.width = (self.image:getWidth() * self._style.scale) + padding.horizontal
+    self.height = (self.image:getHeight() * self._style.scale) + padding.vertical
+
     ---@diagnostic disable-next-line: duplicate-set-field
     self.drawSelf = function()
-        self.image = image
-        if type(image) == 'userdata' and image:typeOf('Image') then
-            self.width = (self.image:getWidth() * self._style.scale) + padding.horizontal
-            self.height = (self.image:getHeight() * self._style.scale) + padding.vertical
-
-            love.graphics.draw(
-                self.image,
-                self.x + self._style.padding[4],
-                self.y + self._style.padding[1],
-                0,
-                self._style.scale,
-                self._style.scale
-            )
-        end
+        love.graphics.draw(
+            self.image,
+            self.x + self._style.padding[4],
+            self.y + self._style.padding[1],
+            0,
+            self._style.scale,
+            self._style.scale
+        )
     end
     return self;
 end
