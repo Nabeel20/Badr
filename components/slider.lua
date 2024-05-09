843ff1e59f9231abd96bc444b70a02797ed53126
Nobel <nabeelandnizam@gmail.com>
2024-05-09 12:22:28 +0300

    refactor and simplify slider

--- components/slider.lua
+++ components/slider.lua
@@ -1,86 +1,89 @@
 local container = require 'badar'
 
 local slider = function(options)
-    options = options or {}
+    options = options or {};
+
+    local self = container(table.spread({
+        id = options.id or 'sliderDefault',
+        width = options.width or 200,
+        height = 6
+    }, options))
     local isPressed = false
-    local handleEvent = function(ii)
-        local _handler = ii:find('handler')
-        local _track = ii:find('track')
 
-        local step = options.step or 1
-        local mousePosition = love.mouse.getX() - ii.globalPosition.x
-        local maxValue = math.max(0, math.min(math.floor(mousePosition / step) * step, ii.width))
-        _handler.x = math.max(0, maxValue) - _handler.width
-        if _handler.x < 0 then
-            _handler.x = 0
-        end
-        _track:style({ visible = true }).width = _handler.x + _handler.width
-        if ii._onValueChange then
-            ii._onValueChange(maxValue)
-        end
-    end
-
-    local track = container({
-            width = options.defaultValue or 0,
-            height = 6,
-            id = 'track'
-        })
-        :style({
+    local styles = {
+        container = {
+            corner = 3,
+            color = { 0.89453125, 0.89453125, 0.89453125, 1 },
+            filled = true,
+        },
+        track = {
             color = options.trackColor or { 0, 0, 0 },
             filled = true,
             corner = 3,
             visible = false,
-        })
-        :modify(function(o)
-            if options.defaultValue then
-                o:style({ visible = true })
-            end
-        end)
-
-    local handler = container({
-            id = 'handler',
-            width = 16,
-            height = 16,
-            x = options.defaultValue or 0
-        })
-        :style({
+        },
+        handler = {
             corner = 8,
             color = { 1, 1, 1 },
             filled = true,
             borderColor = Hex('#4b5563'),
             borderWidth = 1
-        })
-        :modify(function(p)
-            if options.defaultValue then
-                p.x = p.x - 16
-            end
-        end)
-        :onClick(function() isPressed = true end)
-        :onMouseRelease(function() isPressed = false end)
-
-    local props = {
-        id = options.id or 'sliderDefault',
-        width = options.width or 200,
-        height = 6
+        }
     }
-    for key, value in pairs(options) do
-        props[key] = value
+    local props = {
+        track = {
+            width = options.value or 0,
+            height = 8,
+            id = 'track'
+        },
+        handler = {
+            id = 'handler',
+            width = 16,
+            height = 16,
+            x = options.defaultValue or 0
+        }
+    }
+
+    local track = container(props.track).style(styles.track)
+    if options.value then track.style({ visible = true }) end
+
+    local handler = container(props.handler)
+        .style(styles.handler)
+        .onClick(function() isPressed = true end)
+        .onMouseRelease(function() isPressed = false end)
+
+    local handleEvent = function(ii)
+        local _handler = ii.find('handler')
+        local _track = ii.find('track')
+
+        local step = options.step or 1
+        local mousePosition = love.mouse.getX() - ii.globalPosition.x
+        local maxValue = math.max(0, math.min(math.floor(mousePosition / step) * step, ii.width))
+
+        _handler.x = math.max(0, maxValue) - _handler.width
+        if _handler.x < 0 then
+            _handler.x = 0
+        end
+        _track.style({ visible = true }).width = _handler.x + _handler.width
+        if ii.onValuechange_function then
+            ii.onValuechange_function(maxValue)
+        end
     end
-    local component = container(props)
-        :content({ track, handler })
-        :style({
-            corner = 3,
-            color = { 0.89453125, 0.89453125, 0.89453125, 1 },
-            filled = true,
-        })
-        :modify(function(i)
-            i.height = 6
-            local _handler = i:find("handler")
-            _handler.y = (_handler.height - i.height) / 2 * -1
+
+    self.onValueChange = function(func)
+        self.onValuechange_function = func
+        return self
+    end
+    self
+    .style(styles.container)
+    .content({ track, handler }, { alignment = 'center' })
+    .find('handler').x = self.x
+    self
+        .onUpdate(function(i)
+            if isPressed then handleEvent(i) end
         end)
-        :onUpdate(function(ii) if isPressed then handleEvent(ii) end end)
-        :onClick(function(ii) handleEvent(ii) end)
-        :onHover({
+        .onClick(function(i) handleEvent(i) end)
+        .onHover({
             onEnter = function()
                 love.mouse.setCursor(love.mouse.getSystemCursor('hand'))
             end,
@@ -89,11 +92,10 @@ local slider = function(options)
             end
         })
 
-    component.onValueChange = function(_s, func)
-        component._onValueChange = func
-        return component
-    end
-    return component
+
+    return container({ width = self.width, height = 25 })
+        .content({ self }, { alignment = 'center' })
+        .style({ opacity = 0 })
 end
 
 return slider
