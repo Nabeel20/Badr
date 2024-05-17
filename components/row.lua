a341a281ec1e85f13c2022408680bcbdf4220273
Nobel <nabeelandnizam@gmail.com>
2024-05-17 07:32:40 +0300

    add absolute positioning

--- components/row.lua
+++ components/row.lua
@@ -6,21 +6,23 @@ local row = function(children, parent, layout)
     }
     local offset = 0
     for _, child in ipairs(children) do
-        child.x = offset;
-        offset = offset + child.width + (layout.gap or 0)
+        if child.position ~= 'absolute' then
+            child.x = offset;
+            offset = offset + child.width + (layout.gap or 0)
 
-        local alignment = {
-            ['start'] = 0,
-            ['center'] = math.round((parent.height - padding.vertical - child.height) / 2),
-            ['end'] = math.round(parent.height - padding.vertical - child.height)
-        }
-        child.y = alignment[layout.alignment or 'start'];
+            local alignment = {
+                ['start'] = 0,
+                ['center'] = math.round((parent.height - padding.vertical - child.height) / 2),
+                ['end'] = math.round(parent.height - padding.vertical - child.height)
+            }
+            child.y = alignment[layout.alignment or 'start'];
 
-        if child.alignSelf == 'end' then
-            child.x = math.round(parent.width - child.width - padding.horizontal)
-        end
-        if child.alignSelf == 'center' then
-            child.x = math.round((parent.width - child.width - padding.horizontal) / 2)
+            if child.alignSelf == 'end' then
+                child.x = math.round(parent.width - child.width - padding.horizontal)
+            end
+            if child.alignSelf == 'center' then
+                child.x = math.round((parent.width - child.width - padding.horizontal) / 2)
+            end
         end
     end
 
