48aaa022d77937212ce2ca4a27027f5809b0a989
Nobel <nabeelandnizam@gmail.com>
2024-05-17 07:33:39 +0300

    add absolute positioning

--- components/column.lua
+++ components/column.lua
@@ -6,21 +6,23 @@ local column = function(children, parent, layout)
     }
     local offset = 0
     for _, child in ipairs(children) do
-        child.y = offset;
-        offset = offset + child.height + (layout.gap or 0)
+        if child.position ~= 'absolute' then
+            child.y = offset;
+            offset = offset + child.height + (layout.gap or 0)
 
-        local alignment = {
-            ['start'] = 0,
-            ['center'] = math.round((parent.height - padding.horizontal - child.width) / 2),
-            ['end'] = math.round(parent.height - padding.horizontal - child.height)
-        }
-        child.x = alignment[layout.alignment or 'start']
+            local alignment = {
+                ['start'] = 0,
+                ['center'] = math.round((parent.height - padding.horizontal - child.width) / 2),
+                ['end'] = math.round(parent.height - padding.horizontal - child.height)
+            }
+            child.x = alignment[layout.alignment or 'start']
 
-        if child.alignSelf == 'end' then
-            child.y = math.round(parent.height - child.height - padding.vertical)
-        end
-        if child.alignSelf == 'center' then
-            child.y = math.round((parent.height - child.height - padding.horizontal) / 2)
+            if child.alignSelf == 'end' then
+                child.y = math.round(parent.height - child.height - padding.vertical)
+            end
+            if child.alignSelf == 'center' then
+                child.y = math.round((parent.height - child.height - padding.horizontal) / 2)
+            end
         end
     end
 end
